831f369d7767192ee04f20a47f8347ec6b768d0f
krishnapriya <krishnapriyakamesh@gmail.com>
2017-02-07 12:50:57 -0600

    Done with publicresults page with basic style

--- app/controller/publicCtrl.js
+++ app/controller/publicCtrl.js
@@ -1,9 +1,14 @@
 capstone.controller ("PublicCtrl", function ($http,$scope,doctorFactory,$location) {
 
-  // $scope.speciality = doctorFactory.getSpeciality();
-  // $scope.user_insurance = doctorFactory.getInsurance();
+  $scope.speciality = doctorFactory.getSpeciality();
+  // $('#autocomplete-inputSpeciality').autocomplete({
+  //               data: $scope.speciality,
+  //               limit: 10 // The max amount of results that can be shown at once. Default: Infinity.
+  //               });
+  $scope.insurance = doctorFactory.getInsurance();
   $scope.getDoctor = ()=>{
     $scope.user_speciality = doctorFactory.getuserSpeciality($scope.speciality)
+
     console.log($scope.user_speciality)
     $scope.user_insurance = doctorFactory.getuserInsurance($scope.user_insurance)
     console.log($scope.user_insurance)
